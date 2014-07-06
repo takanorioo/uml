0096ff1842ededb79407699fd44e60a92597531d
TakanoriKobashi <takanori.kobashi.10@gmail.com>
2014-07-07 06:29:41 +0900

    first commit

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -15,7 +15,8 @@ class UsersController extends AppController
 {
     public $name = 'Users';
     public $uses = array('User');
-    public $layout = 'login';
+    public $helpers = array('Html', 'Form', 'FormHidden');
+    public $layout = 'base';
 
     /**
      * beforeFilter
@@ -29,18 +30,20 @@ class UsersController extends AppController
         $this->Auth->allow(); //認証なしで入れるページ
     }
 
-     /**
+    /**
      * logout ログアウトページ
      * @param:
      * @author: T.Kobashi
      * @since: 1.0.0
      */
-    public function login()
+    public function logout()
     {
-        // ログイン状態ならトップへ
-        if ($this->me['is_login']) {
-            $this->redirect(array('controller' => '/', 'action' => 'index'));
+        //ログアウトの処理
+        if($this->me['is_login']) {
+            $this->Auth->logout();
+            $this->redirect($this->request->referer()); // 元いたページにリダイレクト
         }
+        $this->redirect(array('controller' => '/', 'action' => 'index'));
     }
 
     /**
@@ -115,19 +118,110 @@ class UsersController extends AppController
     }
 
     /**
-     * logout ログアウトページ
+     * loginCallback Facebook Register後コールバック
      * @param:
      * @author: T.Kobashi
      * @since: 1.0.0
      */
-    public function logout()
+    public function loginCallback()
     {
-        //ログアウトの処理
-        if($this->me['is_login']) {
-            $this->Auth->logout();
-            $this->redirect($this->request->referer()); // 元いたページにリダイレクト
+        $this->facebook = $this->createFacebook();
+
+        $access_token = $this->facebook->getAccessToken();
+        $user_id = $this->facebook->getUser();
+
+        // 正規ルートでこのページに到着した
+        $fb_profile = $this->facebook->api('/me?locale=ja');
+
+        // DBに同じfacebook_user_idがあるかどうか調べる
+        $is_facebook_user = $this->User->findByFacebookUserId($user_id, array('id'));
+
+        if ($is_facebook_user) {
+
+            if (!$this->me['is_login']) {
+
+                // ログアウト状態ならログインさせてリダイレクト
+                $this->Auth->fields = array(
+                    'username' => 'facebook_user_id',
+                    'password' => 'facebook_access_token'
+                );
+
+                $data['User']['id'] = $is_facebook_user['User']['id'];
+                $data['User']['facebook_user_id'] = $user_id;
+                $data['User']['facebook_access_token'] = $access_token;
+
+                if ($this->Auth->login($data['User'])) {
+                    $this->redirect($this->request->referer()); // $this->Auth->redirect(
+                } else {
+                    // ログイン失敗
+                    $this->redirect(array('controller' => 'users', 'action' => 'login'));
+                }
+
+            } else {
+                // 既に同じfacebook_user_idがある場合はトップへ
+                $this->redirect(array('controller' => '/', 'action' => 'index'));
+            }
         }
-        $this->redirect(array('controller' => '/', 'action' => 'index'));
+
+        // トランザクション処理
+        $this->User->begin();
+
+        // DBに会員情報登録
+        $data['User']['facebook_user_id'] = $user_id;
+        $data['User']['facebook_access_token'] = $access_token;
+        $data['User']['first_name'] = $fb_profile['first_name'];
+        $data['User']['last_name'] = $fb_profile['last_name'];
+
+        $this->User->create();
+        if (! $this->User->save($data['User'], true, array('first_name', 'last_name', 'facebook_user_id', 'facebook_access_token'))) {
+            $this->User->rollback();
+            $this->User->invalidate('DB', 'ただいまサーバーが混み合っております。時間をおいてからアクセスしてください。');
+            return;
+        }
+        $user_id = $this->User->id;
+
+        $this->User->commit();
+        // トランザクション処理終わり
+
+        if ($this->me['is_login']) {
+
+            $this->redirect(array('controller' => '/', 'action' => 'index'));
+
+        } else {
+            // ログアウト状態ならログインさせてリダイレクト
+            $this->Auth->fields = array(
+                'username' => 'facebook_user_id',
+                'password' => 'facebook_access_token'
+            );
+            if ($this->Auth->login($data['User'])) {
+
+                $this->redirect($this->Auth->redirect());
+            } else {
+                // ログイン失敗
+                $this->redirect(array('controller' => '/', 'action' => 'login'));
+            }
+        }
     }
 
+    /**
+     * register
+     * @param:
+     * @author: T.Kobashi
+     * @since: 1.0.0
+     */
+    public function register()
+    {
+        if ($this->me['is_login']) {
+            $this->redirect('/');
+        }
+
+        $facebook = $this->createFacebook();
+
+        $option = array(
+            'redirect_uri' => HTTPS_FULL_BASE_URL . '/' . $this->base_dir . '/login/callback',
+        );
+
+        $url = $facebook->getLoginUrl($option);
+        $this->redirect($url);
+    }
 }
