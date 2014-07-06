37727c2bdfa507e6789c487c48f92964bb8ea5d8
TakanoriKobashi <takanori.kobashi.10@gmail.com>
2014-07-07 06:42:06 +0900

    first commit

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -86,6 +86,7 @@ class AppController extends Controller
 
         if (!empty($this->me['is_login'])) {
 
+            pr($this->me);
 
             //ユーザID取得
             $user_id= $this->me['User']['id'];
